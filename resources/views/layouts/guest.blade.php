e1d6de2a0f44ab184ac7a6e68e42e32adcde2af0
dermanefad <fad.derman@ifnti.com>
2024-09-14 11:03:44 +0000

    continuer avec google seulement sur connexion

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -34,16 +34,6 @@
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
-
-               <!-- Bouton Continuer avec Google -->
-                <div class="mt-6">
-                    <a href="{{ route('auth.google') }}" class="w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                        <!-- Logo Google -->
-                        <img src="{{ asset('images/google.png') }}" alt="Google Logo" class="w-5 h-5 mr-2">
-                        Continuer avec Google
-                    </a>
-                </div>
-
             </div>
         </div>
     </div>
