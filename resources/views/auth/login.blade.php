56ad46e7f48fd16278ae2373113c3b25f5f3be8c
dermanefad <fad.derman@ifnti.com>
2024-09-14 11:03:44 +0000

    continuer avec google seulement sur connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,6 +52,14 @@
                     {{ __('Log in') }}
                 </x-primary-button>
             </div>
+            <!-- Bouton Continuer avec Google -->
+            <div class="mt-6">
+                <a href="{{ route('auth.google') }}" class="w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                    <!-- Logo Google -->
+                    <img src="{{ asset('images/google.png') }}" alt="Google Logo" class="w-5 h-5 mr-2">
+                    Continuer avec Google
+                </a>
+            </div>
         </form>
 
     </div>
