0aa5346ac3802a23830575ab9f5f8b7c061d6d8f
dermanefad <fad.derman@ifnti.com>
2024-09-14 13:38:43 +0000

    font image de connexion et d'inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,8 @@
 <x-guest-layout>
     <!-- Ajout de l'image de fond avec Tailwind CSS -->
-    <div class="min-h-screen flex flex-col justify-center items-center bg-cover bg-center" style="background-image: url('{{ asset('images/cut-xl.jpg') }}');">
+    
     <form method="POST" action="{{ route('register') }}" id="menage-form">
         @csrf
-
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,21 +14,9 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans text-gray-900 antialiased">
-
-    <!-- Container principal avec une grille à deux colonnes -->
-    
-    <div class="min-h-screen grid-cols-1 md:grid-cols-2 bg-cover bg-center" style="background-image: url('{{ asset('images/hollywood-md.jpg') }}');">
-    
-    <!--div class="min-h-screen grid grid-cols-1 "-->
-
-        <!-- Section de l'image -->
-        {{-- <div class="hidden md:block">
-            <img src="{{ asset('images/images.jpeg') }}" alt="Description de l'image" class="object-cover w-full h-full">
-        </div> --}}
-
-        <!-- Section du formulaire de connexion -->
-        <div class="flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900 bg-opacity-50 p-6">
+<body class="font-sans text-gray-10000 antialiased">
+    <div class="min-h-screen grid-cols-1 md:grid-cols-2 bg-cover bg-center" style="background-image: url('{{ asset('images/IMG-20200529-WA0154-768x432.jpg') }}');">
+        <div class="flex flex-col justify-center items-center dark:bg-gray-900 bg-opacity-50 p-6">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
