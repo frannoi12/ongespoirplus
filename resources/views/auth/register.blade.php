320fdfb6a97243135a0832fb92e7ff1ca908dda5
dermanefad <fad.derman@ifnti.com>
2024-09-14 13:38:43 +0000

    font image de connexion et d'inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,15 @@
 <x-guest-layout>
     <!-- Ajout de l'image de fond avec Tailwind CSS -->
-    <div class="min-h-screen flex flex-col justify-center items-center bg-cover bg-center" style="background-image: url('{{ asset('images/cut-xl.jpg') }}');">
 
-        <form method="POST" action="{{ route('register') }}" class="bg-white dark:bg-gray-800 bg-opacity-75 p-6 rounded-lg shadow-lg w-full max-w-md">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
-            </div>
+    <form method="POST" action="{{ route('register') }}" id="menage-form">
+        @csrf
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
 
             <!-- Prenom -->
             <div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,16 +17,17 @@
 <body class="font-sans text-gray-900 antialiased">
 
     <!-- Container principal avec une grille à deux colonnes -->
+
     <div class="min-h-screen grid-cols-1 md:grid-cols-2 bg-cover bg-center" style="background-image: url('{{ asset('images/hollywood-md.jpg') }}');">
 
+    <!--div class="min-h-screen grid grid-cols-1 "-->
+
         <!-- Section de l'image -->
-        {{-- <div class="hidden md:block">
         {{-- <div class="hidden md:block">
             <img src="{{ asset('images/images.jpeg') }}" alt="Description de l'image" class="object-cover w-full h-full">
         </div> --}}
 
         <!-- Section du formulaire de connexion -->
-        <div class="flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900 bg-opacity-50 p-6">
         <div class="flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900 bg-opacity-50 p-6">
             <div>
                 <a href="/">
