7bc508bef2d009d7284d43997b23a4365b7a5297
dermanefad <fad.derman@ifnti.com>
2024-09-14 13:38:43 +0000

    font image de connexion et d'inscription

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,10 +17,8 @@
 >>>>>>> 7bea7f9 (login et register)
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-
         <form method="POST" action="{{ route('login') }}" class="bg-white dark:bg-gray-800 bg-opacity-75 p-6 rounded-lg shadow-lg w-full max-w-md">
             @csrf
-
             <!-- Email Address -->
             <div>
                 <x-input-label for="email" :value="__('Email')" />
@@ -28,11 +26,9 @@
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
 <<<<<<< HEAD
             </div>
-
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
-
                 <div class="relative">
                     <x-text-input id="password" class="block mt-1 w-full pr-10"
                                   type="password"
