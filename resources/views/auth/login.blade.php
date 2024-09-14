9e5f98889b516b7bcdde44f109664026263b7ac5
dermanefad <fad.derman@ifnti.com>
2024-09-14 13:38:43 +0000

    font image de connexion et d'inscription

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,21 +34,17 @@
 >>>>>>> 7bea7f9 (login et register)
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-
         <form method="POST" action="{{ route('login') }}" class="bg-white dark:bg-gray-800 bg-opacity-75 p-6 rounded-lg shadow-lg w-full max-w-md">
             @csrf
-
             <!-- Email Address -->
             <div>
                 <x-input-label for="email" :value="__('Email')" />
                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
-
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
-
                 <div class="relative">
                     <x-text-input id="password" class="block mt-1 w-full pr-10"
                                   type="password"
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -140,11 +140,6 @@
 
     <form method="POST" action="{{ route('register') }}" id="menage-form">
         @csrf
-    <div class="min-h-screen flex flex-col justify-center items-center bg-cover bg-center" style="background-image: url('{{ asset('images/cut-xl.jpg') }}');">
-
-        <form method="POST" action="{{ route('register') }}" class="bg-white dark:bg-gray-800 bg-opacity-75 p-6 rounded-lg shadow-lg w-full max-w-md" id="menage-form">
-            @csrf
-
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
